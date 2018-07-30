96fbf9849e3ae81387e937cb9e607bb1a0c51bb0
travisdock <travisdock@gmail.com>
2018-07-30 15:00:44 -0400

    format amount as dollar in serializer

--- app/serializers/entry_serializer.rb
+++ app/serializers/entry_serializer.rb
@@ -1,7 +1,12 @@
 class EntrySerializer < ActiveModel::Serializer
+  include ActionView::Helpers::NumberHelper
   attributes :category, :date, :amount, :notes
 
   def category
     self.object.category.name
+  end
+
+  def amount
+    number_to_currency(self.object.amount)
   end
 end
