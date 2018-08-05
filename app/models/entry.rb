6ea8da5c8990019a11e3d342667f1425e8d71ee3
travisdock <travisdock@gmail.com>
2018-08-04 21:53:01 -0400

    add csv for testing

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -11,8 +11,10 @@ class Entry < ApplicationRecord
   validates :category_id, :presence => true
 
   def self.import
-    file = '/Users/flatironschool/Downloads/data.csv'
-    CSV.foreach(file, headers: true) do |row|
+    csv_text = File.read(Rails.root.join('public', 'data.csv'))
+    csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+    csv.each do |row|
+      byebug
       data = row.to_hash
       @user = User.find(data["user_id"])
       @category = @user.categories.find_by(name: data["category_name"])
