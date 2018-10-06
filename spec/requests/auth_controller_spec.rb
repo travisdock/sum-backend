db4abb573d8765ff52e05c6584ff0188da0e569a
Travis <travisdock@gmail.com>
2018-10-06 11:49:10 -0400

    add factories

--- spec/requests/auth_controller_spec.rb
+++ spec/requests/auth_controller_spec.rb
@@ -1,44 +1,25 @@
 require 'rails_helper'
 require_relative '../support/auth_helper'
 
-# RSpec.describe "Auth", type: :request do
-#     describe 'GET /charts' do
-#         include AuthHelper
-#         before(:each) do
-#             http_login
-#             User.ceate(username: "travis", password: "test", id: 1)
-#         end
-#         # before do
-#         #     basic_auth 'travis', 'test'
-#         #     User.ceate(username: "travis", password: "test", id: 1)
-#         # end
-#         it 'does authenticate' do
-#             get '/api/v1/charts/1'
-    
-#             # test for the 200 status-code
-#             expect(response).to be_success
-#         end
-#     end
-# end
-
 RSpec.describe 'Finances API' do
     include RequestSpecHelper
-    
-  
+
     describe 'GET /charts' do
-      user = User.create(username: "travis", password: "test", email: "test", id: 1)
+
+      let(:user) {create(:user)}
 
       it 'responds with invalid request without JWT' do
-        get '/api/v1/charts/1' 
+        get "/api/v1/charts/#{user.id}"
         expect(response).to have_http_status(401)
-        expect(response.body).to match(/Invalid Request/)
+        expect(response.body).to match(/Token Invalid/)
       end
   
       it 'responds with JSON with JWT' do
         jwt = confirm_and_login_user(user)
-        get '/api/v1/charts/1', headers: { "Authorization" => "Bearer #{jwt}" }
+        get "/api/v1/charts/#{user.id}", headers: { "Authorization" => "#{jwt}" }
         expect(response).to have_http_status(200)
-        expect(json.size).to eq(10)
+        expect(response.body).to match(/No Expenses/)
       end
+
     end
   end
