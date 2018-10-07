3be3b964090d85547d9149783071051f4868cd17
Travis <travisdock@gmail.com>
2018-10-07 15:24:16 -0400

    add user with data factory and add user controller chart tests

--- spec/factories.rb
+++ spec/factories.rb
@@ -6,4 +6,59 @@ FactoryBot.define do
         email { "test@test.com" }
         password { "test" }
     end
+
+    factory :user_with_data, class: 'User' do
+        sequence(:username) { |n| "tester#{n}" }
+        email { "test@test.com" }
+        password { "test" }
+        after(:create) do |user, evaluator|
+            expense_category = create(:expense_category, users: [user])
+            income_category = create(:income_category, users: [user])
+            create_list(:expense, 2, user: user, category: expense_category)
+            create_list(:income, 2, user: user, category: income_category)
+        end
+    end
+
+    factory :category do
+        sequence(:id) { |n| n }
+        factory :expense_category do
+            name { "expense_category" }
+            income { false }
+            untracked { false }
+        end
+        factory :income_category do
+            name { "income_category"}
+            income { true }
+            untracked { false }
+        end
+        factory :untracked_category do
+            name { "untracked_category" }
+            income { false }
+            untracked { true }
+        end
+    end
+
+    factory :entry do
+        sequence(:id) { |n| n }
+        user
+        date { 1.week.ago }
+        amount { 15 }
+        untracked { false }
+        factory :expense do
+            association :category, factory: :expense_category
+            notes { "test expense" }
+            income { false }
+        end
+        factory :income do
+            association :category, factory: :income_category
+            notes { "test income" }
+            income { true }
+        end
+        factory :untracked do
+            association :category, factory: :untracked_category
+            notes { "test untracked" }
+            income { false }
+            untracked { true }
+        end
+    end
 end
--- spec/requests/user_controller_spec.rb
+++ spec/requests/user_controller_spec.rb
@@ -3,5 +3,24 @@ require_relative '../support/auth_helper'
 
 RSpec.describe "User Controller Specs", :type => :request do
   include RequestSpecHelper
+  describe 'GET user correct user charts' do
 
+    let(:user_with_data) {create(:user_with_data)}
+    let(:user_without_data) {create(:user)}
+
+    it 'returns correct format' do
+      jwt = confirm_and_login_user(user_with_data)
+      get "/api/v1/charts/#{user_with_data.id}", headers: { "Authorization" => "#{jwt}" }
+      expect(response).to have_http_status(200)
+      expect(response.body).to match(/\"Total Expense\":\"30.0\"/)
+    end
+
+    it 'returns correct format' do
+        jwt = confirm_and_login_user(user_without_data)
+        get "/api/v1/charts/#{user_without_data.id}", headers: { "Authorization" => "#{jwt}" }
+        expect(response).to have_http_status(200)
+        expect(response.body).to match(/No Expenses/)
+      end
+
+  end
 end
