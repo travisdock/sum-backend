03a773b52a9f7c73851885cca66773f8b6ac8c4b
Travis <travisdock@gmail.com>
2018-10-07 12:07:22 -0400

    finish auth specs, add user controller file

--- spec/requests/auth_controller_spec.rb
+++ spec/requests/auth_controller_spec.rb
@@ -1,25 +1,52 @@
 require 'rails_helper'
 require_relative '../support/auth_helper'
 
-RSpec.describe 'Finances API' do
-    include RequestSpecHelper
+RSpec.describe "Finances API", :type => :request do
+  include RequestSpecHelper
 
-    describe 'GET /charts' do
+  describe 'Login' do
+    let(:user) {create(:user)}
 
-      let(:user) {create(:user)}
+    it 'responds with Username or Password invalid when incorrect login credentials used' do
+      body = {username: "wrong", password: "incorrect"}
+      post "/api/v1/login",
+      params: body.to_json,
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      }
+      expect(response).to have_http_status(401)
+    end
 
-      it 'responds with invalid request without JWT' do
-        get "/api/v1/charts/#{user.id}"
-        expect(response).to have_http_status(401)
-        expect(response.body).to match(/Token Invalid/)
-      end
-  
-      it 'responds with JSON with JWT' do
-        jwt = confirm_and_login_user(user)
-        get "/api/v1/charts/#{user.id}", headers: { "Authorization" => "#{jwt}" }
-        expect(response).to have_http_status(200)
-        expect(response.body).to match(/No Expenses/)
-      end
-
+    it 'responds with a token when correct login credentials used' do
+      body = {username: user.username, password: user.password}
+      post "/api/v1/login",
+      params: body.to_json,
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      }
+      expect(response).to have_http_status(200)
+      expect(response.body).to match(/\"jwt\":/)
     end
   end
+
+  describe 'GET user data' do
+
+    let(:user) {create(:user)}
+
+    it 'responds with invalid request without a valid JWT token' do
+      get "/api/v1/charts/#{user.id}"
+      expect(response).to have_http_status(401)
+      expect(response.body).to match(/Token Invalid/)
+    end
+
+    it 'responds with JSON to a request with a valid JWT' do
+      jwt = confirm_and_login_user(user)
+      get "/api/v1/charts/#{user.id}", headers: { "Authorization" => "#{jwt}" }
+      expect(response).to have_http_status(200)
+      expect(response.body).to match(/No Expenses/)
+    end
+
+  end
+end
