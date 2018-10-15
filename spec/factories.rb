ba0172426ad7c7438532eb1087bc6496810bfba2
Travis <travisdock@gmail.com>
2018-10-15 06:26:39 -0400

    changing around factories to try and get rspec to pass on travis ci

--- spec/factories.rb
+++ spec/factories.rb
@@ -5,22 +5,30 @@ FactoryBot.define do
         sequence(:username) { |n| "tester#{n}" }
         email { "test@test.com" }
         password { "test" }
-    end
-
-    factory :user_with_data, class: 'User' do
-        sequence(:username) { |n| "tester#{n}" }
-        email { "test@test.com" }
-        password { "test" }
-        # after(:create) do |user, evaluator|
-        #     expense_category = create(:expense_category, users: [user])
-        #     income_category = create(:income_category, users: [user])
-        #     create_list(:expense, 2, user: user, category: expense_category)
-        #     create_list(:income, 2, user: user, category: income_category)
+        # factory :user_with_data do
+        #     after(:create) do |user, evaluator|
+        #         expense_category = create(:expense_category, users: [user])
+        #         income_category = create(:income_category, users: [user])
+        #         create_list(:expense, 2, user: user, category: expense_category)
+        #         create_list(:income, 2, user: user, category: income_category)
+        #     end
         # end
     end
 
+    # factory :user_with_data, class: 'User' do
+    #     sequence(:username) { |n| "tester#{n}" }
+    #     email { "test@test.com" }
+    #     password { "test" }
+    #     after(:create) do |user, evaluator|
+    #         expense_category = create(:expense_category, users: [user])
+    #         income_category = create(:income_category, users: [user])
+            # create_list(:expense, 2, user: user, category: expense_category)
+            # create_list(:income, 2, user: user, category: income_category)
+    #     end
+    # end
+
     factory :category do
-        sequence(:id) { |n| Category.maximum(:id) + 1 }
+        sequence(:id) { |n| n }
         factory :expense_category do
             name { "expense_category" }
             income { false }
--- spec/requests/user_controller_spec.rb
+++ spec/requests/user_controller_spec.rb
@@ -5,13 +5,15 @@ RSpec.describe "User Controller Specs", :type => :request do
   include RequestSpecHelper
   describe 'GET user charts' do
 
-    let(:user_with_data) {create(:user_with_data)}
+    let(:user_with_data) {create(:user)}
     let(:user_without_data) {create(:user)}
+    let(:expense_category) {create(:expense_category)}
+    let(:income_category) {create(:income_category)}
     
 
     it 'returns correct format for a user with expenses' do
-      expense_category = create(:expense_category, users: [user_with_data])
-      income_category = create(:income_category, users: [user_with_data])
+      expense_category.users = [user_with_data]
+      income_category.users = [user_with_data]
       create_list(:expense, 2, user: user_with_data, category: expense_category)
       create_list(:income, 2, user: user_with_data, category: income_category)
       jwt = confirm_and_login_user(user_with_data)
@@ -21,21 +23,23 @@ RSpec.describe "User Controller Specs", :type => :request do
     end
 
     it 'returns No Expenses for a user with no expenses' do
-        jwt = confirm_and_login_user(user_without_data)
-        get "/api/v1/charts/#{user_without_data.id}", headers: { "Authorization" => "#{jwt}" }
-        expect(response).to have_http_status(200)
-        expect(response.body).to match(/No Expenses/)
+      jwt = confirm_and_login_user(user_without_data)
+      get "/api/v1/charts/#{user_without_data.id}", headers: { "Authorization" => "#{jwt}" }
+      expect(response).to have_http_status(200)
+      expect(response.body).to match(/No Expenses/)
     end
 
   end
 
   describe 'GET user entries' do
 
-    let(:user_with_data) {create(:user_with_data)}
+    let(:user_with_data) {create(:user)}
+    let(:expense_category) {create(:expense_category)}
+    let(:income_category) {create(:income_category)}
 
     it 'returns all of a users entries in a specific order' do
-      expense_category = create(:expense_category, users: [user_with_data])
-      income_category = create(:income_category, users: [user_with_data])
+      expense_category.users = [user_with_data]
+      income_category.users = [user_with_data]
       create_list(:expense, 2, user: user_with_data, category: expense_category)
       create_list(:income, 2, user: user_with_data, category: income_category)
       jwt = confirm_and_login_user(user_with_data)
