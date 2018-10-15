eb23feb64980dc03c69ddc308c7a97b663ab2a7a
Travis <travisdock@gmail.com>
2018-10-15 05:55:31 -0400

    still trying to fix travis ci tests

--- spec/factories.rb
+++ spec/factories.rb
@@ -20,7 +20,7 @@ FactoryBot.define do
     end
 
     factory :category do
-        sequence(:id) { |n| 3 + n }
+        sequence(:id) { |n| Category.maximum(:id) + 1 }
         factory :expense_category do
             name { "expense_category" }
             income { false }
