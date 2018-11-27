d32a5b3c2dbe1cfdadde246ac48747fe83b9ba79
Travis <travisdock@gmail.com>
2018-11-27 11:27:24 -0700

    add year_view to user table and start test

--- spec/requests/user_controller_spec.rb
+++ spec/requests/user_controller_spec.rb
@@ -7,6 +7,9 @@ RSpec.describe "User Controller Specs", :type => :request do
 
     let(:user_with_data) {create(:user_with_data)}
     let(:user_without_data) {create(:user)}
+    let(:thisyear_user) {create(:user_with_data, year_view: Date.current.year)}
+    let(:lastyear_user) {create(:user_with_data, year_view: 1.year.ago.year)}
+    let(:year_user) {create(:user_with_data, year_view: 2.years.ago.year)}
     
     it 'returns correct format for a user with expenses' do
       jwt = confirm_and_login_user(user_with_data)
@@ -22,6 +25,19 @@ RSpec.describe "User Controller Specs", :type => :request do
       expect(response.body).to match(/No Expenses/)
     end
 
+    it 'returns only entries from the year specified in the user table' do
+      jwt = confirm_and_login_user(user_without_data)
+      get "/api/v1/charts/#{thisyear_user.id}", headers: { "Authorization" => "#{jwt}" }
+      expect(response).to have_http_status(200)
+      entries = JSON.parse(response.body)
+
+      allow(thisyear_user).to receive(:year_test) do |entry|
+        user.year_view == entry.date.year
+      end
+
+      expect(thisyear_user.year_test(entries)).to eq(true)
+    end
+
   end
 
   describe 'GET user entries' do
@@ -34,7 +50,7 @@ RSpec.describe "User Controller Specs", :type => :request do
       expect(response).to have_http_status(200)
       entries = JSON.parse(response.body)
       expect(Date.parse(entries.first['date'])).to be < Date.parse(entries.second['date'])
-      expect(Date.parse(entries.second['date'])).to be < Date.parse(entries.laste['date'])
+      expect(Date.parse(entries.second['date'])).to be < Date.parse(entries.last['date'])
     end
   end
 end
