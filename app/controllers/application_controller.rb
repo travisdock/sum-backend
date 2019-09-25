e5018daa94563b7cd55068a04192c3e153b619b8
Travis <travisdock@gmail.com>
2019-09-25 06:57:26 -0500

    Add v2 endpoint and test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,4 +24,21 @@ class ApplicationController < ActionController::API
     !!current
   end
 
+  private
+  def user_params
+    params.permit(:username, :password, :email)
+  end
+
+  def update_params
+    params.permit(:username, :password, :email, :year_view)
+  end
+
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+
+  def set_year_view
+    @user.year_view = Time.now.year
+    @user.save
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -38,5 +38,8 @@ module PfBackend
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # Load Lib
+    config.autoload_paths << "#{Rails.root}/lib"
   end
 end
